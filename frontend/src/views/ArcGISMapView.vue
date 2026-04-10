3b093b86d90342060537accd9d5d50c75674f2e0
MIGUEL-TGH <hdez.go.miguel@gmail.com>
2026-04-10 14:29:30 -0600

    2026-04-10 2pm

--- frontend/src/views/ArcGISMapView.vue
+++ frontend/src/views/ArcGISMapView.vue
@@ -244,7 +244,7 @@ export default {
       // =========================================================================
       // vue
       dialog_rules: {
-        actived: false,
+        actived: true,
         message: '',
         image: '@/assets/logos/NiñezPrimero-Placa.png'
       },
